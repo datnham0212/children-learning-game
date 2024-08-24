7c1d90257966fc0bae1b8b3525157d19531b7088
datnham0212 <datnham0212@gmail.com>
2024-08-24 21:41:36 +0700

    eighteenth commit

--- components/GameScreen2.tsx
+++ components/GameScreen2.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { useTheme } from './ThemeContext';
 
 export function GameScreen2({ navigation }: { navigation: any }) {
+
+  const { isDarkMode, toggleTheme, themeStyles } = useTheme();
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Picture Guessing</Text>
+    <View style={[styles.container, { backgroundColor: themeStyles.bgColor }]}>
+      <Text style={[styles.text, {color: themeStyles.textColor}]}>Picture Guessing</Text>
       <Button title="Start Game" onPress={() => navigation.navigate("PictureGuessing")} />
     </View>
   );
